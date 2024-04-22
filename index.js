7f72579faf73351d75595275f60f1017e528c42b
kris <nyk.krishnan@gmail.com>
2024-04-23 02:05:13 +0530

    Updated cors

--- index.js
+++ index.js
@@ -22,17 +22,8 @@ const morgan = require("morgan");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// cors
-// const corsOptions = {
-//   origin: /.*.vercel.app.*/g,
-//   credentials: true,
-//   optionsSuccessStatus: 200,
-// };
 app.use(cors());
-
 app.use(morgan("tiny"));
-
 app.use(
   session({
     secret: "your secret key",
